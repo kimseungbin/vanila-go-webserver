c6094e9330264e8b04d6fb21c31606c67eab401b
SeungBin Kim <luxorienskim@gmail.com>
2023-01-20 15:06:09 +0900

    feat: editHandler function

--- main.go
+++ main.go
@@ -39,7 +39,17 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1>\n<div>%s</div>", p.Title, p.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>\n<form action='save/%s' method='post'>\n    <textarea name='body'>%s</textarea>\n    <input type='submit' value='Save'/>\n</form>", p.Title, p.Title, p.Body)
+}
+
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
