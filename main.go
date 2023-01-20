414c3c36cf56e41df2ecbbf97b6742c9192c5cb8
SeungBin Kim <luxorienskim@gmail.com>
2023-01-20 14:13:52 +0900

    feat: viewHandler function
    additionally, merged wiki.go and main.go

--- main.go
+++ main.go
@@ -4,13 +4,42 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() (err error) {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (page *Page, err error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{
+		Title: title,
+		Body:  body,
+	}, nil
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1>\n<div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
